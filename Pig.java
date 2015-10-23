8dfa93d433b29cdb9e83c1a0d076d0c0af49a522
nalam1 <nalam1@s.sfusd.edu>
2015-10-23 08:30:44 -0700

    Update Pig.java

--- Pig.java
+++ Pig.java
@@ -1,4 +1,19 @@
 class Pig implements Animal 
-{     
-     //your code here
+public void setup() {     
+    Pig p = new Pig("cow", "moo");   
+    System.out.println(c.getType() + " goes " + c.getSound());  }  
+}
+class Pig implements Animal {     
+     private String myType;     
+     private String mySound;      
+     public Pig(String type, String sound)     {         
+         myType = type;         
+         mySound = sound;     
+     }     
+     public Pig()     {         
+         myType = "unknown";         
+         mySound = "unknown";     
+     }      
+     public String getSound(){return mySound;}     
+     public String getType(){return myType;} 
 }
