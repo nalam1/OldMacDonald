5de930cf25905909b67c8270188a8ab3e4738f5d
nalam1 <nalam1@s.sfusd.edu>
2015-11-04 19:25:57 -0800

    Update Chick.java

--- Chick.java
+++ Chick.java
@@ -1,4 +1,38 @@
+
 class Chick implements Animal 
 {     
      //your code here
+	 protected String myType;     
+     protected String mySound; 
+     protected String mySound2;     
+        public Chick(String type, String sound)
+    {
+        myType = type;
+        mySound = sound;
+    }
+    public Chick(String type, String soundOne, String soundTwo)
+    {
+        myType = type;
+        if (Math.random() < .5)
+        {
+            mySound = soundOne;
+        }
+        else
+        {
+            mySound = soundTwo;
+        }
+    }
+    public Chick()
+    {
+        myType = "unknown";
+        mySound = "unknown";
+    }
+     public String getSound()
+     {
+        return mySound;
+     }    
+     public String getType()
+     {
+        return myType;
+    }
 }
