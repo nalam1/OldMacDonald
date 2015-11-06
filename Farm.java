8b983b136eb74b06f3da510a06847f9608ba9a11
nalam1 <nalam1@s.sfusd.edu>
2015-11-06 07:50:30 -0800

    Update Farm.java

--- Farm.java
+++ Farm.java
@@ -2,7 +2,7 @@ class Farm  {
    private Animal[] aBunchOfAnimals = new Animal[3];    
    public Farm()     {       
       aBunchOfAnimals[0] = new NamedCow("cow", "Elsie", "moo");           
-      aBunchOfAnimals[1] = new Chick("chick","cluck");       
+      aBunchOfAnimals[1] = new Chick("chick","chirp");       
       aBunchOfAnimals[2] = new Pig("pig","oink");    }         
    public void animalSounds()    {       
      for (int nI=0; nI < aBunchOfAnimals.length; nI++)       {          
